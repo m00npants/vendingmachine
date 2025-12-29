03b0109e086a8cb63d3ad2830b653bcf7df1fc83
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-29 09:52:01 +0100

    Add ConsoleUI for user interaction and integrate VendingMachine functionality

--- src/main/java/org/example/ConsoleUI.java
+++ src/main/java/org/example/ConsoleUI.java
@@ -1,4 +1,60 @@
 package org.example;
 
+import java.util.Scanner;
+
+
 public class ConsoleUI {
+
+    private final IVendingMachine machine;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public ConsoleUI(IVendingMachine machine) {
+        this.machine = machine;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("\n--- Vending Machine ---");
+            System.out.println("1. View products");
+            System.out.println("2. Insert coin");
+            System.out.println("3. Purchase product");
+            System.out.println("4. Return change");
+            System.out.println("5. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showProducts();
+                case 2 -> insertCoin();
+                case 3 -> purchase();
+                case 4 -> returnChange();
+                case 5 -> { return; }
+            }
+        }
+    }
+
+    private void showProducts() {
+        for (Product p : machine.getProducts()) {
+            System.out.println(p.getId() + ": " + p.getDescription() +
+                    " | Price: " + p.getPrice() +
+                    " | Qty: " + p.getQuantity());
+        }
+    }
+
+    private void insertCoin() {
+        System.out.print("Insert coin: ");
+        machine.insertCoin(scanner.nextInt());
+        System.out.println("Balance: " + machine.getBalance());
+    }
+
+    private void purchase() {
+        System.out.print("Enter product ID: ");
+        Product p = machine.purchaseProduct(scanner.nextInt());
+        System.out.println(p == null ? "Purchase failed" : "Purchased: " + p.getName());
+    }
+
+    private void returnChange() {
+        System.out.println("Returned: " + machine.returnChange());
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+     static void main() {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        VendingMachineImpl machine = new VendingMachineImpl();
+
+        machine.addProduct(new Snack(1, "Chips", 20, 5));
+        machine.addProduct(new Beverage(2, "Cola", 15, 4));
+        machine.addProduct(new Fruit(3, "Apple", 10, 6));
+
+        ConsoleUI ui = new ConsoleUI(machine);
+        ui.start();
     }
 }
+
