1e8013b348c5a2f08bbd770ea236d2a40d335376
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-29 09:41:38 +0100

    Add properties and methods to Product class for better product management

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public abstract class Product {
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void reduceQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+    public abstract String getDescription();
 }
