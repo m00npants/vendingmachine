ef598bbf9db30615bae56cd8f52aac34cfa06ea3
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-29 09:34:22 +0100

    Implement VendingMachineImpl class with initial methods for coin insertion, balance retrieval, product purchasing, change return, and product listing

---
+++ src/main/java/org/example/VendingMachineImpl.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.List;
+
+public class VendingMachineImpl implements IVendingMachine {
+
+    @Override
+    public void insertCoin(int coin) {
+
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
+
+    @Override
+    public Product purchaseProduct(int productId) {
+        return null;
+    }
+
+    @Override
+    public int returnChange() {
+        return 0;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return null;
+    }
+}
+
