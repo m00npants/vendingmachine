06f1660fb1fa5c3331197619e16208e8692e208d
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-29 10:17:03 +0100

    Add documentation comments for methods in VendingMachineImpl class

--- src/main/java/org/example/VendingMachineImpl.java
+++ src/main/java/org/example/VendingMachineImpl.java
@@ -7,21 +7,28 @@ import java.util.Set;
 public class VendingMachineImpl implements IVendingMachine {
 
     private final List<Product> products = new ArrayList<>();
+
+    /** Current balance inserted by the user */
     private int balance = 0;
 
     private static final Set<Integer> VALID_COINS = Set.of(1, 2, 5, 10, 20, 50);
 
+ /** Adds a product to the vending machine.*/
     public void addProduct(Product product) {
         products.add(product);
     }
-
+    /**
+     * Inserts a coin into the vending machine.
+     */
     @Override
     public void insertCoin(int coin) {
         if (VALID_COINS.contains(coin)) {
             balance += coin;
         }
     }
-
+    /**
+     * Returns the current balance.
+     */
     @Override
     public int getBalance() {
         return balance;
@@ -42,14 +49,14 @@ public class VendingMachineImpl implements IVendingMachine {
         }
         return null;
     }
-
+/**returns the remaining balance as change and resets the balance to zero.*/
     @Override
     public int returnChange() {
         int change = balance;
         balance = 0;
         return change;
     }
-
+/**returns a list of all products in the vending machine.*/
     @Override
     public List<Product> getProducts() {
         return new ArrayList<>(products);
